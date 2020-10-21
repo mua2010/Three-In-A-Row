ff7ac7eefff49c897f496179ca093aa09542b60e
Umair Afzal <mumairafzal2010@gmail.com>
2020-10-21 16:33:41 -0400

    add proposed fix comment about MVC

--- threeinarow/src/RowGameApp.java
+++ threeinarow/src/RowGameApp.java
@@ -3,6 +3,9 @@ import controller.RowGameController;
 public class RowGameApp {
     /**
      * Starts a new game in the GUI.
+     * MY proposed fix is implemented partially, I will make
+     * necessart changes to keep the view code inside view folder 
+     * same for model and controller
      */
     public static void main(String[] args) {
         RowGameController game = new RowGameController();
