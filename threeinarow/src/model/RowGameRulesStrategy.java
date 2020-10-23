3418628a072ebab1837d7970304e2671249b31e2
Umair Afzal <mumairafzal2010@gmail.com>
2020-10-22 20:21:43 -0400

    add game strategy interface to model

---
+++ threeinarow/src/model/RowGameRulesStrategy.java
@@ -0,0 +1,11 @@
+package model;
+
+public interface RowGameRulesStrategy {
+    public void reset();
+
+    public void move(int row, int col);
+
+    public boolean isWin(int row, int col);
+
+    public boolean isTie();
+}
