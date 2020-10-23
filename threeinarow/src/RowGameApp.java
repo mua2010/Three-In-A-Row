2a9f470bc94087339aa77846dac9153cfb03af47
Umair Afzal <mumairafzal2010@gmail.com>
2020-10-22 20:26:36 -0400

    add working solution for the previous made changes

--- threeinarow/src/RowGameApp.java
+++ threeinarow/src/RowGameApp.java
@@ -1,14 +1,22 @@
 import controller.RowGameController;
+import view.RowGameGUI;
+import model.RowGameModel;
 
 public class RowGameApp {
     /**
-     * Starts a new game in the GUI.
-     * MY proposed fix is implemented partially, I will make
-     * necessart changes to keep the view code inside view folder 
-     * same for model and controller
+     * Starts a new game in the GUI. MY proposed fix is implemented partially, I
+     * will make necessary changes to keep the view code inside view folder. same for
+     * model and controller
      */
     public static void main(String[] args) {
-        RowGameController game = new RowGameController();
-        game.startUp();
+        RowGameModel gameModel = new RowGameModel(); // Observable | PropertyChangeSupport
+        RowGameController gameController = new RowGameController();
+        RowGameGUI gameView = new RowGameGUI(gameModel); // Observers | PropertyChangeListener
+        
+        gameModel.setView(gameView);
+        gameController.setModel(gameModel);
+        gameController.resetModel();
+
+        gameView.getGui().setVisible(true);
     }
 }
