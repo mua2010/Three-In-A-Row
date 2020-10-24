4aa7b638ce93c062691cea8be6efe85c72b0450e
Umair Afzal <mumairafzal2010@gmail.com>
2020-10-24 01:39:44 -0400

    add tictactoe boilerplate

---
+++ threeinarow/src/model/TicTacToeModel.java
@@ -0,0 +1,29 @@
+package model;
+
+public class TicTacToeModel implements RowGameRulesStrategy {
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void move(int row, int col) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isWin(int row, int col) {
+        return false;
+        
+    }
+
+    @Override
+    public boolean isTie() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
