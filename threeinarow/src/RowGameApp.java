0a59fa1165de846feaf1eae0d779aab218b6a039
Umair Afzal <mumairafzal2010@gmail.com>
2020-10-24 01:38:21 -0400

    add commented code for tictactoe gameplay runner

--- threeinarow/src/RowGameApp.java
+++ threeinarow/src/RowGameApp.java
@@ -3,12 +3,31 @@ import view.RowGameGUI;
 import model.RowGameModel;
 
 public class RowGameApp {
+    // enum GameType {
+    //     TicTacToe,
+    //     ThreeInARow
+    // }
     /**
      * Starts a new game in the GUI. MY proposed fix is implemented partially, I
      * will make necessary changes to keep the view code inside view folder. same for
      * model and controller
      */
     public static void main(String[] args) {
+        // String gameType = "";
+        // if (args.length > 0) 
+        //     gameType = args[0];
+        // else {
+        //     System.err.println("Please provide a game type Argument");
+        //     System.exit(1);
+        // }
+        
+        // try {
+        //     gameType = GameType.valueOf(gameType).name();
+        // } catch (IllegalArgumentException iAE) {
+        //     System.err.println("Please provide a valid game type Argument");
+        //     System.exit(1);
+        // }
+        
         RowGameModel gameModel = new RowGameModel(); // Observable | PropertyChangeSupport
         RowGameController gameController = new RowGameController();
         RowGameGUI gameView = new RowGameGUI(gameModel); // Observers | PropertyChangeListener
